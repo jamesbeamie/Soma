2a4b24eb03644d8e6486f66d7625b5f2ef1f1af3
James <admin@James.local>
2020-02-27 00:22:08 +0300

    binding

--- Soma/Soma/ContentView.swift
+++ Soma/Soma/ContentView.swift
@@ -7,10 +7,32 @@
 //
 
 import SwiftUI
+struct PushButton: View{
+    let title: String
+    @Binding var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
+    var body: some View{
+        Button(title){
+            self.isOn.toggle()
+        }
+    .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors: offColors), startPoint:  .top, endPoint: .bottom))
+        .foregroundColor(.white)
+    .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
 
 struct ContentView: View {
+    @State private var rememberMe = false
     var body: some View {
-        Text("Soma App")
+        VStack{
+            PushButton(title: "remember me", isOn: $rememberMe)
+            Text(rememberMe ? "ON" : "OFF")
+        }
     }
 }
 
