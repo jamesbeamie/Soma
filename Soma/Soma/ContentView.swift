885aa927c00dad2d4dab4ae651ddbb7e2605cb3d
James <admin@James.local>
2020-02-27 01:12:18 +0300

    Environment

--- Soma/Soma/ContentView.swift
+++ Soma/Soma/ContentView.swift
@@ -7,31 +7,22 @@
 //
 
 import SwiftUI
-struct PushButton: View{
-    let title: String
-    @Binding var isOn: Bool
-    
-    var onColors = [Color.red, Color.yellow]
-    var offColors = [Color(white: 0.6), Color(white: 0.4)]
-    
-    var body: some View{
-        Button(title){
-            self.isOn.toggle()
-        }
-    .padding()
-        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors: offColors), startPoint:  .top, endPoint: .bottom))
-        .foregroundColor(.white)
-    .clipShape(Capsule())
-        .shadow(radius: isOn ? 0 : 5)
-    }
-}
 
 struct ContentView: View {
-    @State private var rememberMe = false
+    @Environment(\.horizontalSizeClass) var sizeClass
     var body: some View {
-        VStack{
-            PushButton(title: "remember me", isOn: $rememberMe)
-            Text(rememberMe ? "ON" : "OFF")
+        if sizeClass == .compact {
+            return AnyView(VStack{
+                Text("Active size Class")
+                Text("Compact")
+            }
+            .font(.largeTitle))
+        } else {
+            return AnyView(HStack {
+                Text("Active size Class")
+                Text("Regular")
+            }
+            .font(.largeTitle))
         }
     }
 }
