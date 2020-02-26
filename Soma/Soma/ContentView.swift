4c1ae4d1de29cc8513381b6e6a54beaf01e9cbff
James <admin@James.local>
2020-02-26 23:48:29 +0300

    initial commit

---
+++ Soma/Soma/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Soma
+//
+//  Created by James on 2/26/20.
+//  Copyright © 2020 James. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Soma")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
